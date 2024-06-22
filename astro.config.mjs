60bf5e434ce64356311252584dbd6c4eb52f62ed
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-06-22 07:52:35 -0400

    Acd sitemap generator

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
 import alpinejs from "@astrojs/alpinejs";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [alpinejs()]
+  site: "https://www.succubus-bdsm.com",
+  integrations: [alpinejs(), sitemap()],
 });
+
