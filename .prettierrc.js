11250c48496ab18738abba5683e71a98b76cccf8
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2025-03-15 14:42:27 -0400

    Update prettier config file

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,3 +1,12 @@
 export default {
   tabWidth: 2,
+  plugins: ["prettier-plugin-astro"],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
 };
